3a8a3a325d23d1d273457cb53f30f69892abe8a4
Tariq Almazyad <Tariq.almazyad@hotmail.com>
2020-09-27 11:47:38 +0300

    added layer  send package

--- OnMyWay/HomeController.swift
+++ OnMyWay/HomeController.swift
@@ -16,10 +16,45 @@ class HomeController: UIViewController {
         let label = UILabel()
         label.text = "Welcome to \nOnMyWay "
         label.numberOfLines = 0
-        label.font = UIFont.init(name: "AvenirNext-Heavy", size: 24)
+        label.font = UIFont.init(name: "AvenirNext-Heavy", size: 32)
         label.textColor = .black
         label.setHeight(height: 100)
+        label.textAlignment = .left
         return label
+    }()
+    
+    
+    private lazy var travelContainerView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 20
+        imageView.image = #imageLiteral(resourceName: "rosebox-BFdSCxmqvYc-unsplash")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var sendPackageContainerView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 20
+        imageView.image = #imageLiteral(resourceName: "sendPackagePhoto")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [travelContainerView, sendPackageContainerView])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 16
+        stackView.setHeight(height: 300)
+        return stackView
+    }()
+    
+    private lazy var bottomSendPackageView: UIView = {
+        let view = UIView()
+        view.setHeight(height: 200)
+        return view
     }()
     
     override func viewDidLoad() {
@@ -29,12 +64,29 @@ class HomeController: UIViewController {
         configureUI()
     }
     
-    func configureUI(){
-        view.backgroundColor = .white
-        view.addSubview(titleLabel)
-        titleLabel.centerX(inView: view, topAnchor: view.topAnchor, paddingTop: 16)
+    override func viewDidLayoutSubviews() {
+        bottomSendPackageView.layer.configureGradientBackground( UIColor.clear.cgColor, UIColor.black.cgColor)
     }
     
+    func configureUI(){
+        view.backgroundColor = #colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9058823529, alpha: 1)
+        view.addSubview(titleLabel)
+        titleLabel.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 16)
+        titleLabel.anchor(left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 18)
+        view.addSubview(stackView)
+        stackView.centerX(inView: view, topAnchor: titleLabel.bottomAnchor, paddingTop: 16)
+        stackView.anchor(left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 12, paddingRight: 12)
+        sendPackageContainerView.addSubview(bottomSendPackageView)
+        bottomSendPackageView.anchor(left: sendPackageContainerView.leftAnchor, bottom: sendPackageContainerView.bottomAnchor, right: sendPackageContainerView.rightAnchor)
+        sendPackageContainerView.layoutIfNeeded()
+        
+    }
+    
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .darkContent
+    }
     
     func logout(){
         do {
@@ -50,7 +102,7 @@ class HomeController: UIViewController {
         if Auth.auth().currentUser?.uid == nil {
             self.presentLoggingController()
         } else {
-//            fetchUser()
+            //            fetchUser()
         }
     }
     
